3649ba3e53e453d58be51dab4de10edfbd9d99dc
Bri616 <bridotson@gmail.com>
2016-04-24 11:47:29 -0700

    add draft mode, draft list, and publish button

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,23 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
 
+
+def post_draft_list(request):
+        posts = Post.objects.filter(published_date__isnull=True
+                                    ).order_by('created_date')
+        return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+                                ).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -17,12 +27,12 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -37,3 +47,9 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_publish(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.publish()
+        return redirect('blog.views.post_detail', pk=pk)
